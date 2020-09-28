f5615d75acb8d7a07b1c3aac53d9c21b43040549
Sagar Makhija <sagar1309@live.com>
2020-09-29 01:21:27 +0530

    initial setup
    - adds dummy .env
    - adds config.js to expose env vars
    - adds Vuetify and changes in vuetify config
    - adds favicons
    - fix eslint-prettier issue
    - adds jshintrc to resolve es6 features
    - adds dummy DashboardNavbar
    - remove unused components
    - rename Home to Dashboard

---
+++ src/components/DashboardNavbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dashboard-navbar">
+    <v-app-bar app color="primary" elevate-on-scroll>
+      <div class="d-flex align-center white--text">{{ appName }}</div>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DashboardNavbar",
+  props: {
+    appName: {
+      default: "",
+      type: String
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <DashboardNavbar :app-name="appName" />
+
+    <v-main class="mb-5">Main content goes here</v-main>
+  </div>
+</template>
+
+<script>
+import DashboardNavbar from "@/components/DashboardNavbar.vue";
+import config from "@/config.js";
+
+export default {
+  name: "Home",
+  components: {
+    DashboardNavbar
+  },
+  computed: {
+    appName() {
+      return config.appTitle();
+    }
+  }
+};
+</script>
